4f5442ef37df9d359507a5b0bc994df845a1ab5b
Hernán Rubio <hernan.matias.rubio@gmail.com>
2021-10-24 16:58:14 -0300

    Se añade la función consultas_por_palabras()

--- search_engine.py
+++ search_engine.py
@@ -1,6 +1,7 @@
 
 from datetime import *
 import json
+from _ast import Break
 
 def search_engine():
     print("\nBuscador de información recopilada...\
@@ -8,16 +9,21 @@ def search_engine():
     \
     \n1.\tConsultas por fecha y hora.\
     \n2.\tConsultas de palabras o frases.\
+    \n3.\tFinalizar.\
     \
     \n\nOpción número: ")
     
-    opcion_elegida = input()
-    
-    switcher = {
-        1: consultas_por_fechahora()
-        }
-    
-    switcher.get(opcion_elegida)
+    while True:
+        try: opcion_elegida = int(input())
+        except ValueError: opcion_elegida = 0
+        
+        if (opcion_elegida == 1): consultas_por_fechahora(); break
+        
+        elif (opcion_elegida == 2): consultas_por_palabras(); break
+            
+        elif (opcion_elegida == 3): break
+            
+        else: print("Opción incorrecta. Ingrese nuevamente...")
 
 def consultas_por_fechahora():
     print("\nIngrese los siguientes datos por favor...\
@@ -52,12 +58,34 @@ def consultas_por_fechahora():
     print("\nProcesando...")
     procesar_archivo(nombre_usuario, cantidad_tuits, list_fechas, list_horas)
 
+def consultas_por_palabras():
+    print("\nIngrese los siguientes datos por favor...\
+    \
+    \ni.\tCantidad de tuits (m primeros tuits)\
+    \nii.\tConsulta a realizar (por ejemplo: Adrián and Bussiness)\
+        \n\t\tOpcional.*")
+    
+    print("\nCantidad de tuits:")
+    while True:
+        try: 
+            cantidad_tuits = int(input())
+            if cantidad_tuits <= 0: raise ValueError 
+            break
+        except ValueError: print("Ingrese cantidad de tuits válido...")
+    
+    print("\nIngrese su consulta (operadores permitidos OR, AND, NOT)...")
+    while True:    
+        text = input()
+        query = text.split(" ") if len(text) > 0 else text
+        if (len(query) != 0): break
+        print("Debe ingresar una consulta")
+
 def procesar_archivo(*args):
     with open("data.json", "r", encoding = "utf-8") as file:
         for i in file:
             objeto = json.loads(i)
             print(objeto)
 
-procesar_archivo()
-# search_engine()
+#procesar_archivo()
+search_engine()
 
